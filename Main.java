325a24d8101d19cc79e3409f0c8a2ca8bf9a686c
douvryf <douvryf@yahoo.fr>
2020-04-21 14:40:20 +0200

    Update Main.java - Hello les pupuces

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Francois, vive les pupuces");
+        System.out.println("Hello les pupuces");
     }
 }
 
